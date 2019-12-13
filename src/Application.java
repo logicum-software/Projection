9bc34d00b5b6fbd628fed7389046e1d7a6297e98
Markus Gärtner <gaertner.brilon@gmail.com>
2019-12-13 06:11:50 +0100

    20191213 06:10 MG

--- src/Application.java
+++ src/Application.java
@@ -7,5 +7,10 @@ public class Application {
     public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
 
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        f.setSize(400,300);
+        f.setLocationRelativeTo(null);
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        f.setLayout(null);//using no layout managers
+        f.setVisible(true);//making the frame visible
     }
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -10,10 +10,4 @@ public class MainFrame extends JFrame {
         public void actionPerformed(ActionEvent e){
         }
     });
-
-        f.setSize(400,300);
-        f.setLocationRelativeTo(null);
-        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        f.setLayout(null);//using no layout managers
-        f.setVisible(true);//making the frame visible
 }
